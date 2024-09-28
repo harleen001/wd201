9e86b0a1f742e38ad91bbc5ffc0e14d280b3b9da
harleen <harleen.johal31@gmail.com>
2024-09-28 11:45:58 +0530

    routes-fixed

--- http-server/index.js
+++ http-server/index.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+ /*const fs = require("fs");
 
 fs.writeFile(
     "sample.txt",
@@ -32,3 +32,41 @@ fs.writeFile(
     if (err) throw err;
     console.log("File test.txt deleted successfully!");
   });
+  */
+
+const http = require("http");
+const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
