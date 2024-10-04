ffde2fb65922dfbe86de90295fac9632d6888de4
harleen <harleen.johal31@gmail.com>
2024-10-04 22:59:21 +0530

    test_case_nodependncy

--- todo-cli/__tests__/first.js
+++ todo-cli/__tests__/first.js
@@ -10,8 +10,19 @@ const todoList = require('../todo_app');
 const {all, markAsComplete, add} = todoList()
 
 describe('Todolist Test Suite', () => 
-   {  test('Should add new todo',() => {
-  expect(all.length).toBe(0);
+   
+   { beforeAll(()=>{
+      add(
+         {
+            title: "Test todo",
+            completed: false,
+            dueDate: new Date().toISOString().slice(0, 10)
+         })
+   })
+      
+      test('Should add new todo',() => {
+ // expect(all.length).toBe(0);
+ const todoItemsCount = all.length;
   add(
    {
       title: "Test todo",
@@ -19,6 +30,23 @@ describe('Todolist Test Suite', () =>
       dueDate: new Date().toISOString().slice(0, 10)
    }
   );
-  expect(all.length).toBe(1);  
-     })
+  expect(all.length).toBe(todoItemsCount+1);  //now second test case doesnt depend on first
+     });
+
+     test('Should mark a todo as complete',() => {
+      expect(all[0].completed).toBe(false);
+      markAsComplete(0);
+      expect(all[0].completed).toBe(true);  
+         })
+    
+//the second test case depends on first so we will now use beforeall()
+
+
+
+
+
+
+
+
+
   })
